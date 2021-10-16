db07bddc425c655f838800b3e1e65fa33dce3a52
Amal Ragh K <78066884+AmalRaghk@users.noreply.github.com>
2021-10-16 20:03:08 +0530

    Create string functions.java

---
+++ string functions.java
@@ -0,0 +1,10 @@
+/*
+length()-to find the size of a string
+cocat()-to concat a string
+equals()-to compare string
+equalsIgnoreCase()-to compare strings without case difference
+compareTo()-Unicode character from each String is compared
+indexOf()- we want to know the index of the first occurence of a character in a string
+charAt()- method returns the character located at a String‘s specified index.
+subString()-The substring begins with the character at the specified index and extends to the end of the string.
+*/
