fea27ce4db75fa2c7d1cb93558788dc1513dab08
Amal Ragh K <78066884+AmalRaghk@users.noreply.github.com>
2021-10-17 20:22:44 +0530

    Update string functions.java

--- string functions.java
+++ string functions.java
@@ -7,4 +7,32 @@ compareTo()-Unicode character from each String is compared
 indexOf()- we want to know the index of the first occurence of a character in a string
 charAt()- method returns the character located at a String‘s specified index.
 subString()-The substring begins with the character at the specified index and extends to the end of the string.
+toUpperCase(): returns the string value converted to uppercase
+toLowerCase(): returns the string value converted to lowercase
 */
+public class Review {
+  
+  public static void main(String[] args) {
+    String name="github";
+    //to print the size of the string
+    System.out.println(name.length());
+    //to print a concated string the string doesn't change
+    System.out.println(name.concat("repo"));
+    //to print the index of a char
+    System.out.println(name.indexOf("u"));
+    //to print the character at an index
+    System.out.println(name.charAt(2));
+    //to check equals
+    System.out.println(name.equals("github"));
+    //to print the substring from index 3 onto
+    System.out.println(name.substring(3));
+    //to print the upper case
+    System.out.println(name.toUpperCase());
+    //to print the lower case
+    System.out.println(name.toLowerCase());
+    
+    
+    
+  }
+  
+}
